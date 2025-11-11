684f81699e970a4842b7ec6ea76e90c5b791f81f
lokeshgujjar6655 <lokeshgujjar6655@gmail.com>
2025-11-11 15:20:21 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import telebot
 import os
 
 TOKEN = os.getenv("8305742502:AAHsdQ69n-JWxvShkpn6YFLm431PmuZ2OXc")
-bot = telebot.TeleBot(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)
+bot = telebot.TeleBot"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"
 
 @bot.message_handler(commands=['start'])
 def start(message):
